9efb779dff015ec46bea3304c96c3aa40046a8b3
Prajwal Shah <prajwalshah1721@gmail.com>
2021-03-21 13:54:55 +0530

    Design changes

--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -7,7 +7,7 @@ export const HomePage = () => {
     return (
         <div>
             <iframe _ngcontent-c12="" class="ng-tns-c12-3" bnresponsivevideo="" frameborder="0" src={`${vid}?autoplay=1&controls=0&loop=0`} style={{
-                "width": "1309px", "height": "736px", "data - ready": true
+                "width": "100%", "height": "736px", "data - ready": true
             }} allowFullScreen="true" controls="false"></iframe>
             <About />
             <WhatsNew />
