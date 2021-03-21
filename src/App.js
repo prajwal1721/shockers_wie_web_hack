19c515fa3edaaec080a4562278e49fb214451c93
Prajwal Shah <prajwalshah1721@gmail.com>
2021-03-21 12:21:07 +0530

    Routing community

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { NavbarLeft } from './components/Navbar/navbarLeft';
 import { HomePage } from './pages/homepage/homepage';
 import { useState } from 'react';
 import './App.css';
+import { Community } from './pages/Communitypage/Community';
 import { createBrowserHistory } from "history";
 import { Router, Redirect, Route, Switch } from 'react-router-dom';
 const hist = createBrowserHistory();
@@ -12,7 +13,7 @@ function App() {
         <NavbarLeft />
 
         <Switch>
-          <Route path='/community' exact component={HomePage} />
+          <Route path='/community' exact component={Community} />
           <Route path='/whatsnew' exact component={HomePage} />
           <Route path='/products' exact component={HomePage} />
           <Route path='/about' exact component={HomePage} />
